9f66a87479b2055bb84b702524df57e52fae7e8f
吟梦 <i@iidream.gq>
2015-03-09 19:48:07 +0800

    照顾手机端=A=

--- header.php
+++ header.php
@@ -107,6 +107,20 @@ box-shadow: 0 1px 6px rgba(0,0,0,.12),0 1px 6px rgba(0,0,0,.12);
   </style>
 </head>
 <body>
+<?php
+function is_mobile() {
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+$mobile_agents = Array("240x320","acer","acoon","acs-","abacho","ahong","airness","alcatel","amoi","android","anywhereyougo.com","applewebkit/525","applewebkit/532","asus","audio","au-mic","avantogo","becker","benq","bilbo","bird","blackberry","blazer","bleu","cdm-","compal","coolpad","danger","dbtel","dopod","elaine","eric","etouch","fly ","fly_","fly-","go.web","goodaccess","gradiente","grundig","haier","hedy","hitachi","htc","huawei","hutchison","inno","ipad","ipaq","ipod","jbrowser","kddi","kgt","kwc","lenovo","lg ","lg2","lg3","lg4","lg5","lg7","lg8","lg9","lg-","lge-","lge9","longcos","maemo","mercator","meridian","micromax","midp","mini","mitsu","mmm","mmp","mobi","mot-","moto","nec-","netfront","newgen","nexian","nf-browser","nintendo","nitro","nokia","nook","novarra","obigo","palm","panasonic","pantech","philips","phone","pg-","playstation","pocket","pt-","qc-","qtek","rover","sagem","sama","samu","sanyo","samsung","sch-","scooter","sec-","sendo","sgh-","sharp","siemens","sie-","softbank","sony","spice","sprint","spv","symbian","tablet","talkabout","tcl-","teleca","telit","tianyu","tim-","toshiba","tsm","up.browser","utec","utstar","verykool","virgin","vk-","voda","voxtel","vx","wap","wellco","wig browser","wii","windows ce","wireless","xda","xde","zte");
+$is_mobile = false;
+foreach ($mobile_agents as $device) {
+if (stristr($user_agent, $device)) {
+$is_mobile = true;
+break;
+}
+}
+return $is_mobile;
+}
+?>
 <header>
  <div class="navbar navbar-fixed-top navbar-inverse" role="navigation" style="opacity:.7;"> 
  <div class="container">
